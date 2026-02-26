235c141a8557f17e4f601c17a68699b1f48d53cb
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-02-26 22:45:52 +0200

    Add routing and login functionality with styles
    - Integrated react-router-dom for navigation between Home and Login pages.
    - Created Login component with form handling and state management.
    - Added CSS styles for the Login page layout and design.
    - Updated package.json and package-lock.json to include react-router-dom dependency.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
 import './Login.css';
+import logo from '../../assets/logo.png';
+import { useState } from 'react';
 
 const Login = () => {
+  const [signState, setSignState] = useState("Login");
+
+
   return (
     <div className='login'>
-      
+      <img src={logo} alt="Netflix Logo" className="login-logo" />
+      <div className="login-container">
+        <form className='login-form'>
+          <h1>{signState}</h1>
+          {signState === "Register" ? <input type="text" placeholder='Your name' /> : <></>}
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          <button>{signState}</button>
+
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+
+            <p>Need Help?</p>
+          </div>
+
+          <div className="form-switch">
+            {signState === "Login" ? <p>New to Netflix? <span onClick={() => setSignState("Register")}>Register</span></p> : <p>Already have an account? <span onClick={() => setSignState("Login")}>Login</span></p>}
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
