19740b7ca802e6187ab2654b89abaed219b026b4
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-02 18:49:00 +0200

    Enhance TitleCards component to fetch and display movie data from API
    - Added state management to TitleCards for API data retrieval.
    - Updated the component to fetch movie data based on category using The Movie Database API.
    - Modified card rendering to display movie titles and images from the API instead of static data.
    - Updated Home page to pass appropriate categories to TitleCards for dynamic content.

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,21 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
 
-const TitleCards = ({title, category}) => {
+const TitleCards = ({ title, category }) => {
+
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef(null);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjc2Mzg0MTM1NmU3NTM0MzhiNTkwZmU3YTAzZWZmMCIsIm5iZiI6MTc1MTcyMDQ0Mi40NTI5OTk4LCJzdWIiOiI2ODY5MjFmYTRjYzc3ZjY4YzBlZDUwZWEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.isCazwf1DJBAoe1R8zfm_GK2No9yxHcIfpQ2BEeoSMs'
+    }
+  };
+
+  
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -11,17 +23,22 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel)
   }, []);
 
   return (
     <div className='title-cards'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return <div className="card" key={index}>
-            <img src={card.image} alt={card.name} />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} alt={card.original_title} />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
