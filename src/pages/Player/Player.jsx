cdf9e2210f53276a6cd2bd6277bdc6bc642c0a7f
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-04 21:48:46 +0200

    Add Player page and enhance Navbar with scroll effect
    - Introduced Player component for video playback, fetching data from The Movie Database API based on movie ID.
    - Updated App component to include routing for the Player page.
    - Enhanced Navbar to change background color on scroll for better visibility.
    - Modified TitleCards to link to the Player page for each movie card.
    - Added new styles for Player component and Navbar transitions.

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
 import './Player.css';
+import back_arrow_icon from '../../assets/back_arrow_icon.png';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Player = () => {
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: ""
+  });
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjc2Mzg0MTM1NmU3NTM0MzhiNTkwZmU3YTAzZWZmMCIsIm5iZiI6MTc1MTcyMDQ0Mi40NTI5OTk4LCJzdWIiOiI2ODY5MjFmYTRjYzc3ZjY4YzBlZDUwZWEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.isCazwf1DJBAoe1R8zfm_GK2No9yxHcIfpQ2BEeoSMs'
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results[0]))
+      .catch(err => console.error(err));
+  });
+
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="Back Arrow" onClick={() => navigate(-1)} />
+      <iframe width={"90%"} height={"90%"} src={`https://www.youtube.com/embed/${apiData.key}`} title='Trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
